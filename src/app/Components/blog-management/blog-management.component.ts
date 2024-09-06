0f860568d75bc6b475b59535dea1499f029773b0
Mariam Gamal <mariamgamal4646@gmail.com>
2024-09-07 00:14:46 +0300

    added blog.json

---
+++ src/app/Components/blog-management/blog-management.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Blog } from './blog';
+import { BlogService } from './blog.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-blog-management',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './blog-management.component.html',
+  styleUrl: './blog-management.component.css',
+})
+export class BlogManagementComponent implements OnInit {
+  constructor(private blogService: BlogService) {}
+
+  allblogs: Blog[] = [];
+  ngOnInit(): void {
+    this.blogService.getAll().subscribe((data) => {
+      this.allblogs = data;
+    });
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { PatientsTableComponent } from './Components/patients/patients-table/patients-table.component';
 import { PatientProfileComponent } from './Components/Pages/patient-profile/patient-profile.component';
-
+import { BlogManagementComponent } from './Components/blog-management/blog-management.component';
+import { CreateComponent } from './Components/blog-management/create/create.component';
 export const routes: Routes = [
-
-  
-        { path: '', component: DashboardComponent },
-        { path: 'patients', component: PatientsTableComponent },
-        { path: 'patients-profile', component: PatientProfileComponent },
-        // Add other routesPatientProfileComponent
- ];
+  { path: '', component: DashboardComponent },
+  { path: 'patients', component: PatientsTableComponent },
+  { path: 'patients-profile', component: PatientProfileComponent },
+  { path: 'blog-management', component: BlogManagementComponent },
+  { path: 'blog/create', component: CreateComponent },
+  // Add other routesPatientProfileComponent
+];
