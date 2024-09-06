26e821772ac3d2eff862365b806c523c61df1ce1
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-09-06 22:19:18 +0300

    add more details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { PatientsTableComponent } from './Components/patients/patients-table/patients-table.component';
 import { PatientProfileComponent } from './Components/Pages/patient-profile/patient-profile.component';
+import { DepartmentComponent } from './Components/department/department.component';
 
 export const routes: Routes = [
-
-  
+               
         { path: '', component: DashboardComponent },
         { path: 'patients', component: PatientsTableComponent },
         { path: 'patients-profile', component: PatientProfileComponent },
+        { path: 'department' , component: DepartmentComponent }
         // Add other routesPatientProfileComponent
  ];
