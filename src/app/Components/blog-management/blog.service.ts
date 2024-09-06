22dbf0e54e46d19f67e1d0564fd995eefebb7029
Mariam Gamal <mariamgamal4646@gmail.com>
2024-09-07 00:18:55 +0300

    modified blog.service component

---
+++ src/app/Components/blog-management/blog.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Blog } from './blog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogService {
+  constructor(private httpClient: HttpClient) {}
+
+  getAll() {
+    return this.httpClient.get<Blog[]>('http://localhost:3000/blogs');
+  }
+
+  create(data: Blog) {
+    return this.httpClient.post('http://localhost:3000/blogs', data);
+  }
+}
