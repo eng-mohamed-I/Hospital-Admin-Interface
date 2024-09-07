6e609ad92f07ddc90fddefd024e94cdfc6640f26
Mariam Gamal <mariamgamal4646@gmail.com>
2024-09-07 18:31:02 +0300

    added blogedit component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PatientsTableComponent } from './Components/patients/patients-table/pat
 import { PatientProfileComponent } from './Components/Pages/patient-profile/patient-profile.component';
 import { BlogManagementComponent } from './Components/blog-management/blog-management.component';
 import { CreateComponent } from './Components/blog-management/create/create.component';
+import { EditComponent } from './Components/blog-management/edit/edit.component';
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'patients', component: PatientsTableComponent },
   { path: 'patients-profile', component: PatientProfileComponent },
   { path: 'blog-management', component: BlogManagementComponent },
   { path: 'blog/create', component: CreateComponent },
+  { path: 'blog/edit/:id', component: EditComponent },
   // Add other routesPatientProfileComponent
 ];
