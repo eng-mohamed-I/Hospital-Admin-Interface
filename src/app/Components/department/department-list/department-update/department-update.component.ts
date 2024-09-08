35564b7abc606ed00620a77b60baca2f1de55b18
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-09-08 07:46:19 +0300

    handle comfirmation message

---
+++ src/app/Components/department/department-list/department-update/department-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './department-update.component.html',
+  styleUrl: './department-update.component.css'
+})
+export class DepartmentUpdateComponent {
+
+}
