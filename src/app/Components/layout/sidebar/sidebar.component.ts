4925bf82dd05c09db91c09a7e06f540b59e00597
MohamedYassin <144175425+eng-mohamed-I@users.noreply.github.com>
2024-09-25 20:13:28 +0300

    handle side bar using role

--- src/app/Components/layout/sidebar/sidebar.component.ts
+++ src/app/Components/layout/sidebar/sidebar.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AdminLoginService } from '../../../services/admin/admin-login.service';
+import { BehaviorSubject } from 'rxjs';
+import { DoctorLoginService } from '../../../services/doctor/doctor-login.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,13 +13,22 @@ import { AdminLoginService } from '../../../services/admin/admin-login.service';
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  isLogged: any;
+  role: any;
   constructor(
-    private _authDoctorService: AdminLoginService,
-    private _router: Router
-  ) {}
+    private _authAdminService: AdminLoginService,
+    private _router: Router,
+    private _authDcotorService: DoctorLoginService
+  ) {
+    this.isLogged = new BehaviorSubject<boolean>(
+      this._authAdminService.isUserLogedIn
+    );
+    this.role = this._authAdminService.getAdminRole();
+    console.log(this.role);
+  }
 
   logOut() {
-    this._authDoctorService.logOut();
+    this._authAdminService.logOut();
     this._router.navigateByUrl('login');
   }
 }
