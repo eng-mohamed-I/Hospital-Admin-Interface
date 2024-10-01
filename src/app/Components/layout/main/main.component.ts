320092f905ab040670b3b67c1e454ad86b373c6b
MohamedYassin <144175425+eng-mohamed-I@users.noreply.github.com>
2024-10-01 09:36:13 +0300

    respoinsive design

--- src/app/Components/layout/main/main.component.ts
+++ src/app/Components/layout/main/main.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 
@@ -8,8 +13,6 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
