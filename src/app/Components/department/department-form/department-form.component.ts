6a78c8a7089c7b69c45c6caa384b7d854a30bef8
MohamedYassin <144175425+eng-mohamed-I@users.noreply.github.com>
2024-10-01 11:19:04 +0300

    add doctors to department

--- src/app/Components/department/department-form/department-form.component.ts
+++ src/app/Components/department/department-form/department-form.component.ts
@@ -1,36 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { DepartmentService } from '../../../services/department/department.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-department-form',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './department-form.component.html',
-  styleUrls: ['./department-form.component.css']
+  styleUrls: ['./department-form.component.css'],
 })
 export class DepartmentFormComponent implements OnInit {
-
   departmentForm: FormGroup;
   selectedDoctors: any[] = []; // Array to store selected doctors
   doctors: any[] = []; // Array to store fetched doctors
-  message: string = ""
+  message: string = '';
 
-  constructor(private _router: Router, private fb: FormBuilder, private departmentService: DepartmentService) {
+  constructor(
+    private _router: Router,
+    private fb: FormBuilder,
+    private departmentService: DepartmentService
+  ) {
     this.departmentForm = this.fb.group({
-      departmentName: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
-      departmentDescription: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
-      doctor: ['']
+      departmentName: [
+        '',
+        [Validators.required, Validators.pattern('[a-zA-Z ]*')],
+      ],
+      departmentDescription: [
+        '',
+        [Validators.required, Validators.pattern('[a-zA-Z ]*')],
+      ],
+      doctor: [''],
     });
   }
 
   ngOnInit(): void {
-    this.getDoctors()
+    this.getDoctors();
   }
-  
+
   // get all doctors
   getDoctors(): void {
     this.departmentService.getDoctors().subscribe({
@@ -39,15 +52,17 @@ export class DepartmentFormComponent implements OnInit {
       },
       error: (err) => {
         console.error('err fetch doctors:', err);
-      }
+      },
     });
   }
 
   // add selected doctor
   addDoctor(): void {
-    const selectedDoctorId = this.departmentForm.get('doctor')?.value
+    const selectedDoctorId = this.departmentForm.get('doctor')?.value;
     if (selectedDoctorId) {
-      const selectedDoctor = this.doctors.find(doctor => doctor.name === selectedDoctorId);
+      const selectedDoctor = this.doctors.find(
+        (doctor) => doctor.name === selectedDoctorId
+      );
 
       // Check if the doctor is already added
       if (selectedDoctor && !this.selectedDoctors.includes(selectedDoctor)) {
@@ -65,44 +80,43 @@ export class DepartmentFormComponent implements OnInit {
   }
 
   // this for clear message
-  clearMessage(messaeg: string) { 
+  clearMessage(messaeg: string) {
     setTimeout(() => {
-      this.message = ""
-    },2000)
+      this.message = '';
+    }, 2000);
   }
 
   // Handle form submission
   onSubmit(): void {
+    console.log(this.departmentForm.value);
     if (this.departmentForm.valid) {
       const departmentData = {
         name: this.departmentForm.get('departmentName')?.value,
         description: this.departmentForm.get('departmentDescription')?.value,
-        doctors: this.selectedDoctors.map(doc => doc._id)
+        doctors: this.selectedDoctors.map((doc) => doc._id),
       };
-      console.log(departmentData)
+      console.log(departmentData);
       this.departmentService.addDepartment(departmentData).subscribe({
         next: (response) => {
           // show success message
-          this.message = response.message
+          this.message = response.message;
           this.departmentForm.reset();
           this.selectedDoctors = [];
 
           // clear message
-          this.clearMessage(this.message)
+          this.clearMessage(this.message);
           setTimeout(() => {
-            this._router.navigate(["/department/list"])
-          },2000)
+            this._router.navigate(['/department/list']);
+          }, 2000);
         },
         error: (err) => {
           // show error message
-          this.message = err.error.message
-          
+          this.message = err.error.message;
+
           // clear message
-          this.clearMessage(this.message)
-        
-        }
+          this.clearMessage(this.message);
+        },
       });
     }
   }
-
 }
--- src/app/services/department/department.service.ts
+++ src/app/services/department/department.service.ts
@@ -3,34 +3,40 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DepartmentService {
   private baseUrl = 'http://localhost:5000/api/departments';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   addDepartment(departmentData: any): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/create`,departmentData);
+    return this.http.post<any>(`${this.baseUrl}/create`, departmentData);
   }
 
   deleteDepartment(departmentId: string): Observable<any> {
     return this.http.delete<any>(`${this.baseUrl}/${departmentId}`);
   }
-  
+
   getDepartments(): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/`);
   }
 
-  getDapartmentById(id: any): Observable<any> { 
-    return this.http.get<any>(`${this.baseUrl}/${id}`)
+  getDapartmentById(id: any): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${id}`);
   }
 
-  updateDepartment(id : any, department: any): Observable<any> { 
-    return this.http.put<any>(`${this.baseUrl}/${id}`, department)
+  updateDepartment(id: any, department: any): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/${id}`, department);
   }
 
-  getDoctors(): Observable<any> { 
-    return this.http.get<any>("http://localhost:5000/api/doctors/")
+  getDoctors(): Observable<any> {
+    return this.http.get<any>('http://localhost:5000/api/doctors/');
+  }
+
+  getDepartmentDoctors(id: any): Observable<any> {
+    return this.http.get<any>(
+      `http://localhost:5000/api/doctors/department/${id}`
+    );
   }
 }
